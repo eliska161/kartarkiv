f9113df755e9737142599db23b8fdfa11ea7fd04
Elias Skaug-Danielsen <eliasskaugdanielsen10@gmail.com>
2025-10-23 19:59:07 +0200

    Update file upload instructions and accepted formats

--- client/src/components/AddMapModal.tsx
+++ client/src/components/AddMapModal.tsx
@@ -838,13 +838,13 @@ const AddMapModal: React.FC<AddMapModalProps> = ({ isOpen, onClose, mapToEdit, o
                     ? 'Slipp filene her...' 
                     : mapToEdit 
                       ? 'Dra filer hit eller klikk for å legge til nye filer' 
-                      : 'Dra filer hit eller klikk for å laste opp PDF eller bildefiler'
+                      : 'Dra filer hit eller klikk for å laste opp Ocad, PDF eller bildefiler'
                   }
                 </p>
                 <input
                   type="file"
                   multiple
-                  accept=".pdf,.jpg,.jpeg,.png,.gif"
+                  accept=".pdf,.jpg,.jpeg,.png,.gif,.ocd"
                   onChange={handleFileChange}
                   className="hidden"
                   id="file-upload"
